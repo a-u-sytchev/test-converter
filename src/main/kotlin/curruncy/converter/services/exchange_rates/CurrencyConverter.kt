562370e335689e3377726d403e7162e70861dd42
a.sytchev@ <>
2024-05-17 10:43:53 +0300

    Добавил преобразование рассчитаной суммы в денежный формат

--- src/main/kotlin/curruncy/converter/services/exchange_rates/CurrencyConverter.kt
+++ src/main/kotlin/curruncy/converter/services/exchange_rates/CurrencyConverter.kt
@@ -2,18 +2,22 @@ package curruncy.converter.services.exchange_rates
 
 import curruncy.converter.domain.CalculatedCurrencyCost
 import curruncy.converter.domain.ExchangeRates
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.text.DecimalFormat
 import kotlin.math.round
 import kotlin.math.roundToLong
 
 /**
- * Конвертирует указанную сумму в каждую валюту и возвращает перечень стоимости целевой валюты по отношению к остальным.
- * @param amount Сумма, котора будет конвертирована
+ * Расчитывает сколько можно купить каждой валюты на указанную сумму целевой валюты.
+ * @param amount Сумма целевой валюты, для которой расчитывается конвертация
  */
 fun ExchangeRates.convertAmount(amount: Double): CalculatedCurrencyCost {
     val convertedRates: MutableMap<String, Double> = mutableMapOf()
     for (rate in this.rates) {
-        convertedRates[rate.key] = rate.value * amount
+        // С помощью BigDecimal приводим значение к денежному формату.
+        convertedRates[rate.key] = BigDecimal(rate.value * amount)
+            .setScale(2, RoundingMode.FLOOR).toDouble()
     }
     return CalculatedCurrencyCost(this.currency, convertedRates.toMap())
 }
