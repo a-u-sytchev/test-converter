4e100b6dec8d8183746bb7e729f60b1b4a58d28e
Alexander Zhuravlev <a.zhuravlev@ekomobile.ru>
2024-05-17 14:51:15 +0300

    Review.

--- src/main/kotlin/curruncy/converter/domain/CalculatedCurrencyCost.kt
+++ src/main/kotlin/curruncy/converter/domain/CalculatedCurrencyCost.kt
@@ -2,5 +2,5 @@ package curruncy.converter.domain
 
 data class CalculatedCurrencyCost(
     val currency: String,
-    val cost: Map<String, Double>
+    val cost: Map<String, Double> // TODO: Возможно, стоит сразу везде использовать BigDecimal
 )
