64d4c314af1e591225fad94bdf267feb85ea20d0
Alexander Zhuravlev <a.zhuravlev@ekomobile.ru>
2024-05-22 00:06:00 +0300

    Review 3.

--- src/main/kotlin/curruncy/converter/domain/currency/CurrencyUnit.kt
+++ src/main/kotlin/curruncy/converter/domain/currency/CurrencyUnit.kt
@@ -3,6 +3,9 @@ package curruncy.converter.domain.currency
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonNames
 
+/**
+ * FIXME: name и minSize нигде не используются. Их не обязательно маппить из ответа API.
+ */
 @Serializable
 data class CurrencyUnit(
     val id: String,
